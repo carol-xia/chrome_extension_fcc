8c1d9d3ca3861a55bcf7d5164a8fc9eae2cc46aa
Dennishly <103156776+Dennishly@users.noreply.github.com>
2022-06-15 23:37:18 -0700

    hackathon w/ carol

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-async function fetchData() {
-    const res = await fetch ("https://api.coronavirus.data.gov.uk/v1/data");
-    const record = await res.json();
-    console.log(record);
-    document.getElementById("date").innerHTML=record.data[0].date;
-    document.getElementById("areaName").innerHTML=record.data[0].areaName;
-    document.getElementById("latestBy").innerHTML=record.data[0].latestBy;
-    document.getElementById("deathNew").innerHTML=record.data[0].deathNew;
+// async function fetchData() {
+//     const res = await fetch ("https://api.coronavirus.data.gov.uk/v1/data");
+//     const record = await res.json();
+//     console.log(record);
+//     document.getElementById("date").innerHTML=record.data[0].date;
+//     document.getElementById("areaName").innerHTML=record.data[0].areaName;
+//     document.getElementById("latestBy").innerHTML=record.data[0].latestBy;
+//     document.getElementById("deathNew").innerHTML=record.data[0].deathNew;
+// }
+// fetchData();
+
+function togglePokemonList(){
+  document.getElementById("pokemon-types").classList.toggle("show");
 }
-fetchData();
