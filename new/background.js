d45bb8c24fd42e39771beb02e6869735fcbb1976
Carol Xia <c.xia.98@gmail.com>
2022-06-16 08:49:17 -0700

    adding files

---
+++ new/background.js
@@ -0,0 +1,19 @@
+chrome.tabs.onActivated.addListener(tab => {
+    chrome.tabs.get(tab.tabId, current_tab_info => {
+        // console.log(current_tab_info.url)
+        // if (/^https:\/\/www\.codesmith/.test(current_tab_info.url)){
+        if (/^https:\/\/www\.codesmith\.io\/about/.test(current_tab_info.url)){
+            // console.log(current_tab_info.url)
+            chrome.tabs.insertCSS(null, {file: './styles.css'});
+            chrome.tabs.executeScript(null, {file: './foreground.js'}, () => console.log('i inject'))
+        }
+    });
+});
+
+// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//     if(request.message === 'check the storage'){
+//         chrome.storage.local.get("password", value => {
+//             console.log(value);
+//         })
+//     }
+// })
---
+++ new/foreground.js
@@ -0,0 +1,26 @@
+// document.querySelector('.lnXdpd').classList.add('spinspin');
+const picArray = document.querySelectorAll('.team-item img');
+picArray.forEach(el => {
+  el.classList.add('spinspin');
+})
+
+// const first = document.createElement('button');
+// first.innerText = "SET DATA";
+// first.id = "first";
+
+// const second = document.createElement('button');
+// second.innerText = "SHOUTOUT TO BACKEND";
+// second.id = "second";
+
+// document.querySelector('body').appendChild(first);
+// document.querySelector('body').appendChild(second);
+
+// first.addEventListener('click', () => {
+//     chrome.storage.local.set({"password": "123"});
+//     console.log("I SET DATA");
+// })
+
+// second.addEventListener('click', () => {
+//     chrome.runtime.sendMessage({message: 'check the storage'});
+//     console.log('I sent the message');
+// })
